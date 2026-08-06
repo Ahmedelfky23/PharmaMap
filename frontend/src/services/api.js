c9cb26fdeb298304b01cb9dbb139d79fc9551083
Ahmedelfky23 <elfkyahmed234@gmail.com>
2026-08-06 06:31:44 +0300

    feat: initialize axios instance with base URL for API communication

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pharmamap-60d8.onrender.com",
+  baseURL: "https://pharmamap-60d8.onrender.com/api",
 });
 
 export default api;
